aed94c45f810ec14fcf987205cea9d267d31ded0
Joseph Black <jblack@vailsys.com>
2021-03-05 16:04:59 -0600

    Change environment variable scope and add additional checks

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # Initialization
+if [[ -z "$AWS_REGION" ]]; then
+    echo "Missing AWS region. No deployments will be made."
+    exit 1
+fi
+if [[ -z "$GITHUB_REPOSITORY_SLUG" ]]; then
+    echo "Missing GitHub repository slug, e.g. octocat/hello-world. No deployments will be made."
+    exit 2
+fi
 if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]]; then
     echo "Missing AWS credentials. No deployments will be made."
-    exit 1
-else
-    if [[ -z "$PERSONAL_ACCESS_TOKEN" ]]; then
-        echo "Missing GitHub credentials. No deployments will be made."
-        exit 2
-    else
-        if [[ -z "$NPM_AUTH_TOKEN" ]]; then
-            echo "Missing NPM credentials. No deployments will be made."
-            exit 3
-        fi
-    fi
+    exit 3
 fi
-
-echo "Starting deployment..."
+if [[ -z "$PERSONAL_ACCESS_TOKEN" ]]; then
+    echo "Missing GitHub credentials. No deployments will be made."
+    exit 4
+fi
+if [[ -z "$NPM_AUTH_TOKEN" ]]; then
+    echo "Missing NPM credentials. No deployments will be made."
+    exit 5
+fi
 
 # Get tag information
 PREVIOUS_GIT_TAG=${$(git describe --tags --abbrev=0)#v} # removes the 'v' at the beginning of the tag, e.g. v1.2.3 -> 1.2.3
@@ -26,8 +30,11 @@ node compare-versions.js $PREVIOUS_GIT_TAG $VERSION_IN_CHANGELOG
 
 if [[ $? -ne 0 ]]; then
     echo "Version numbers were not valid. No deployments will be made."
-    exit 4
+    exit 6
 fi
+
+# Begin deployment
+echo "Starting deployment..."
 
 # Push package to NPM
 yarn install --frozen-lockfile --production=false
